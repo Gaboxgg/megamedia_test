2fbba4e1f742328b8d71e464c9b602eeee7d387f
Gabriel Gaslonde <ggaslonde@transtecnia.cl>
2024-07-04 16:41:57 -0400

    fixing firebase call, adjusting xmls , improve adapter

--- app/src/main/java/com/example/megamediatest/adapters/FilesAdapter.kt
+++ app/src/main/java/com/example/megamediatest/adapters/FilesAdapter.kt
@@ -1,13 +1,21 @@
 package com.example.megamediatest.adapters
 
 import android.content.Context
+import android.graphics.text.LineBreaker.JUSTIFICATION_MODE_INTER_WORD
+import android.net.Uri
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.MediaController
 import android.widget.TextView
+import android.widget.VideoView
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
+import com.bumptech.glide.request.RequestOptions
 import com.example.megamediatest.R
 import com.example.megamediatest.data.filePojo
 import java.util.*
@@ -20,29 +28,45 @@ class MyRecyclerViewAdapter internal constructor( context: Context?,
     private val mInflater: LayoutInflater = LayoutInflater.from(context)
     private var mClickListener: ItemClickListener? = null
 
-    // inflates the row layout from xml when needed
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view: View = mInflater.inflate(R.layout.item_file, parent, false)
         return ViewHolder(view)
     }
 
-    // binds the data to the TextView in each row
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val file = mFiles[position]
+
         holder.myTextView.text = "${file.title}  \n \n  ${file.description} \n \n"
-        Glide.with(holder.itemView).load(file.img).into(holder.myImageView)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            holder.myTextView.justificationMode = JUSTIFICATION_MODE_INTER_WORD
+        }
+
+        Glide.with(holder.itemView)
+            .load(file.img)
+            .transition(withCrossFade())
+            .into(holder.myImageView)
+
+
+        val uri: Uri = Uri.parse(file.url)
+        holder.myVideoView.setVideoURI(uri)
+
+        val mediaController = MediaController(holder.itemView.context)
+        mediaController.setAnchorView(holder.myVideoView)
+        mediaController.setMediaPlayer(holder.myVideoView)
+
+        holder.myVideoView.setMediaController(mediaController)
     }
 
-    // total number of rows
     override fun getItemCount(): Int {
         return mFiles.size
     }
 
-    // stores and recycles views as they are scrolled off screen
     inner class ViewHolder internal constructor(itemView: View) : RecyclerView.ViewHolder(itemView),
         View.OnClickListener {
         var myTextView: TextView = itemView.findViewById<TextView>(R.id.text)
         var myImageView: ImageView = itemView.findViewById<ImageView>(R.id.image)
+        var myVideoView: VideoView = itemView.findViewById<VideoView>(R.id.videoView)
 
         init {
             itemView.setOnClickListener(this)
@@ -53,17 +77,14 @@ class MyRecyclerViewAdapter internal constructor( context: Context?,
         }
     }
 
-    // convenience method for getting data at click position
     fun getItem(id: Int): filePojo {
         return mFiles[id]
     }
 
-    // allows clicks events to be caught
     fun setClickListener(itemClickListener: ItemClickListener?) {
         this.mClickListener = itemClickListener
     }
 
-    // parent activity will implement this method to respond to click events
     interface ItemClickListener {
         fun onItemClick(view: View?, position: Int)
     }
